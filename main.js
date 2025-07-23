da0396cecb59c496219cb5ecc130777ec439999b
Caetano Bordin <hyperplayer2012@gmail.com>
2025-07-23 15:25:03 -0300

    Finalização do projeto

--- main.js
+++ main.js
@@ -23,5 +23,29 @@ function gotResults(error, results) {
         img1 = document.getElementById('alien2');
         img2 = document.getElementById('alien3');
         img3 = document.getElementById('alien4');
+        if(results[0].label == "Palmas") {
+            img.src = 'aliens-01.gif'
+            img1.src = 'aliens-02.png'
+            img2.src = 'aliens-03.png'
+            img3.src = 'aliens-04.png'
+        }
+        else if(results[0].label == "Tambor de bexiga") {
+            img.src = 'aliens-01.png'
+            img1.src = 'aliens-02.gif'
+            img2.src = 'aliens-03.png'
+            img3.src = 'aliens-04.png'
+        }
+        else if(results[0].label == "Estalos") {
+            img.src = 'aliens-01.png'
+            img1.src = 'aliens-02.png'
+            img2.src = 'aliens-03.gif'
+            img3.src = 'aliens-04.png'
+        }
+        else {
+            img.src = 'aliens-01.png'
+            img1.src = 'aliens-02.png'
+            img2.src = 'aliens-03.png'
+            img3.src = 'aliens-04.gif'
+        }
     }
 }
